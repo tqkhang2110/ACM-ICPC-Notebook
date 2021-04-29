56da2f4a5e4b9962dc292fff6147bbfe33db4cad
TQKhang <83112813+tqkhang2110@users.noreply.github.com>
2021-04-29 13:14:39 +0700

    Create Template.cc

---
+++ Template.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int INF = numeric_limits<int>::max();
+const int MOD = 1000000007;
+const int MAX = 100010;
+
+struct Solve 
+{
+	int n;
+	int a[MAX];
+	
+	void work() {
+		
+	}
+};
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	Sieve::calc();
+	int t;
+	cin >> t;
+	while(t--) {
+		Solve().work();
+	}
+	return 0;
+}
