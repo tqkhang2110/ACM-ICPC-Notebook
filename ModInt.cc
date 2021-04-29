72afcef6f5da8bf2db23453bad47f42647d23f0b
TQKhang <83112813+tqkhang2110@users.noreply.github.com>
2021-04-29 14:27:07 +0700

    Update ModInt.cc

--- ModInt.cc
+++ ModInt.cc
@@ -7,30 +7,30 @@ const int MOD = 1000000007;
 const int MAX = 100010;
 
 template<int MOD> struct ModInt {
-   using Mint = ModInt;
-   int val;
-   ModInt(long long __val = 0) { fix(__val % MOD + MOD); }
-   Mint& fix(int __val) { val = __val; if (val >= MOD) val -= MOD; return *this; }
-   explicit operator int() { return val; }
-   Mint operator + (const Mint &mt) const { return Mint().fix(val + mt.val); }
-   Mint operator - (const Mint &mt) const { return Mint().fix(val - mt.val + MOD); }
-   Mint operator * (const Mint &mt) const { return Mint().fix((long long)val * mt.val % MOD + MOD); }
-   Mint& operator += (const Mint &mt) { return *this = *this + mt; }
-   Mint& operator -= (const Mint &mt) { return *this = *this - mt; }
-   Mint& operator *= (const Mint &mt) { return *this = *this * mt; }
-   Mint pow(int y) const {
-      Mint x = *this;
-      Mint ans(1);
-      for (; y > 0; y >>= 1, x *= x) {
-         if (y & 1) ans *= x;
-      }
-      return ans;
-   }
-   Mint operator -() const { return Mint().fix(MOD - val); }
-   Mint inv() const { return pow(MOD - 2); }
-   Mint operator / (const Mint &mt) const { return *this * mt.inv(); }
-   Mint& operator /= (const Mint &mt) const { return *this / mt; }
-   friend ostream& operator << (ostream& os, const Mint& mt) { return os << mt.val; }
+	using Mint = ModInt;
+	int val;
+	ModInt(long long __val = 0) { fix(__val % MOD + MOD); }
+	Mint& fix(int __val) { val = __val; if (val >= MOD) val -= MOD; return *this; }
+	explicit operator int() { return val; }
+	Mint operator + (const Mint &mt) const { return Mint().fix(val + mt.val); }
+	Mint operator - (const Mint &mt) const { return Mint().fix(val - mt.val + MOD); }
+	Mint operator * (const Mint &mt) const { return Mint().fix((long long)val * mt.val % MOD + MOD); }
+	Mint& operator += (const Mint &mt) { return *this = *this + mt; }
+	Mint& operator -= (const Mint &mt) { return *this = *this - mt; }
+	Mint& operator *= (const Mint &mt) { return *this = *this * mt; }
+	Mint pow(int y) const {
+		Mint x = *this;
+		Mint ans(1);
+		for (; y > 0; y >>= 1, x *= x) {
+			if (y & 1) ans *= x;
+		}
+		return ans;
+	}
+	Mint operator -() const { return Mint().fix(MOD - val); }
+	Mint inv() const { return pow(MOD - 2); }
+	Mint operator / (const Mint &mt) const { return *this * mt.inv(); }
+	Mint& operator /= (const Mint &mt) const { return *this / mt; }
+	friend ostream& operator << (ostream& os, const Mint& mt) { return os << mt.val; }
 };
 
 using Mint = ModInt<MOD>;
